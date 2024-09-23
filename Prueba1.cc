fc563dec29eaafe7bb6bd81c91ad0cfcebb4c0ba
GabrielGuerrero24 <00112824@uca.edu.sv>
2024-09-23 14:54:49 -0600

    feat:Practicar1

---
+++ Prueba1.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main (){
+    std:: string palabra; 
+    float numero1 ;
+
+   std:: cout << " Dia uno estudiando" << "/n"; 
+   std:: cout << "Ingrese una palabra: "; 
+   std:: cin >> palabra;
+   std::  cout << "ingrse dos numeros"<< "\n";
+   std:: cin >> numero1 ;  
+
+
+
+
+
+
+    return 0; 
+}
